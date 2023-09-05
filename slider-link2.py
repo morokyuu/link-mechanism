0674c76d7a6fef922a23187112f23e7967eccaa0
zota <zota@example.com>
2023-09-05 22:59:22 +0900

    hachi-noji

--- slider-link2.py
+++ slider-link2.py
@@ -18,8 +18,8 @@ x = r * np.cos(th)
 y = r * np.sin(th)
 alpha = np.arctan(y/Lx - x)
 
-xl = L + x
-yl = np.zeros((1,NUM))
+# xl = L + x
+# yl = np.zeros((1,NUM))
 
 l = np.sqrt((Lx - x)**2 + (0 - y)**2)
 
@@ -28,33 +28,33 @@ py = (L - l)*np.sin(-alpha)
 
 
 
-fig,ax = plt.subplots()
-ax.scatter(x,y)
-ax.scatter(Lx,0)
-# ax.scatter(xl,yl)
-ax.scatter(px,py)
-ax.set_aspect('equal')
-ax.grid()
+# fig,ax = plt.subplots()
+# ax.scatter(x,y)
+# ax.scatter(Lx,0)
+# # ax.scatter(xl,yl)
+# ax.scatter(px,py)
+# ax.set_aspect('equal')
+# ax.grid()
 
 
 
 
-mat = np.vstack([x,y,xl,yl,th,alpha])
+mat = np.vstack([x,y,px,py,th,alpha])
 dat = mat.T
 
 
-# def drawLine(ax,x0,y0,x1,y1):
-#     ax.plot(np.array([x0,x1]),np.array([y0,y1]))
+def drawLine(ax,x0,y0,x1,y1):
+    ax.plot(np.array([x0,x1]),np.array([y0,y1]))
 
-# for n,m in enumerate(dat[:-1,:]):
-#     fig,ax = plt.subplots()
-#     ax.scatter(m[0],m[1])
-#     ax.scatter(m[2],m[3])
-#     drawLine(ax, m[0],m[1],m[2],m[3])
-#     ax.set_xlim([-0.1,0.3])
-#     ax.set_ylim([-0.1,0.1])
-#     ax.set_aspect('equal')
-#     ax.grid()
-#     plt.savefig(f'{n}.png')
+for n,m in enumerate(dat[:-1,:]):
+    fig,ax = plt.subplots()
+    ax.scatter(m[0],m[1])
+    ax.scatter(m[2],m[3])
+    drawLine(ax, m[0],m[1],m[2],m[3])
+    ax.set_xlim([-0.1,0.3])
+    ax.set_ylim([-0.1,0.1])
+    ax.set_aspect('equal')
+    ax.grid()
+    plt.savefig(f'{n}.png')
 
 
