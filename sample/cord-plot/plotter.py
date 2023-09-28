afadb0f549c1c10bc14f31cc0b8f29b932de74d0
zota <zota@example.com>
2023-09-29 01:14:41 +0900

    set position

--- sample/cord-plot/plotter.py
+++ sample/cord-plot/plotter.py
@@ -9,11 +9,32 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def rotZ(th):
+    return np.array([
+        [ np.cos(th), np.sin(th), 0.0],
+        [-np.sin(th), np.cos(th), 0.0],
+        [          0,          0, 1.0]
+        ])
+
+def tr(x,y):
+    return np.array([
+        [        1.0,          0,   x],
+        [          0,        1.0,   y],
+        [          0,          0, 1.0]
+        ])
+
 df = pd.read_csv("foot.txt",sep="\t")
 
+foot = np.vstack((df['x'],df['y'],np.ones(df.shape[0])))
+
+foot = np.hstack((foot, np.vstack((df['x'].head(1),df['y'].head(1),1))))
+
+foot = tr(109,250) @ rotZ(-np.pi) @ foot
+
 fig,ax = plt.subplots()
+ax.plot(foot[0], foot[1])
+ax.grid()
 
-ax.plot(df['x'],df['y'])
-
-
-
+print("x,y")
+for x,y in zip(foot[0],foot[1]):
+    print(f"{x:.3f},{y:.3f}")
